dd3ac70e43cf7c9af149d42105d609fbff337944
Matteo Galvagni <matteo.galvagni@synclab.it>
2022-11-23 17:25:46 +0100

    working docker container with apache

--- docker/relaunch_docker.sh
+++ docker/relaunch_docker.sh
@@ -1,3 +1,4 @@
-sudo docker build -t docker-test .
-sudo docker run --name docker-test -p 6969:80 docker-test
-sudo docker rm docker-test
+sudo docker build -t docker-test -f docker-frontend/Dockerfile .
+sudo docker run -td --name docker-test -p 6969:80 docker-test
+#add -td to run on background
+

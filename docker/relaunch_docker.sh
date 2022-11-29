f74c0a093ed60f9f396cfd19bb90804b3067458b
Matteo Galvagni <matteo.galvagni@synclab.it>
2022-11-29 20:58:35 +0100

    working docker

--- docker/relaunch_docker.sh
+++ docker/relaunch_docker.sh
@@ -1,13 +1,13 @@
-sudo docker build -t docker-backend-python -f docker-backend-python/Dockerfile .
+sudo docker build -t docker-backend-python --build-arg port=8000 -f docker-backend-python/Dockerfile .
 sudo docker run --device /dev/snd:/dev/snd --net=host -td --name docker-backend-python docker-backend-python
 
-sudo docker build -t docker-backend-main -f docker-backend-main/Dockerfile .
+sudo docker build -t docker-backend-main --build-arg port=8080 -f docker-backend-main/Dockerfile .
 sudo docker run --net=host -td --name docker-backend-main docker-backend-main
 
-sudo docker buildx build -t docker-backend-rtsp -f docker-RTSPtoWeb/Dockerfile .
+sudo docker buildx build -t docker-backend-rtsp --build-arg port=8083 -f docker-RTSPtoWeb/Dockerfile .
 sudo docker run --net=host -td --name docker-backend-rtsp docker-backend-rtsp
 
-sudo docker build -t docker-frontend -f docker-frontend/Dockerfile .
+sudo docker build -t docker-frontend --build-arg port=820 -f docker-frontend/Dockerfile .
 sudo docker run --net=host -td --name docker-frontend docker-frontend
 
 
